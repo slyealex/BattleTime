56e966707c0a98f67916a809de2bd573846b952a
ALEX SLYE <slyealex@live.com>
2016-04-14 15:44:35 -0400

    fixed routing issue preventing creation of users, tested and working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,20 +10,20 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path notice: 'You have signed up!'
+      redirect_to root_path, notice: 'You have signed up!'
     else
       render :new
     end
   end
 
   def edit
-    
+
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :firstname, :lastname, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :address, :firstname, :lastname, :password, :password_confirmation)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  get 'stores/new'
-
-  get 'stores/index'
-
-  get 'events/new'
-
-  get 'events/index'
-
-  get 'sessions/new'
-
-  resources :users
-  resources :sessions
 
   root 'home#home'
 
-  end
+  resources :users
+  resources :sessions
+  resources :stores
+  resources :events
+
+end
