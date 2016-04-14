5776f09085db00479a413df08760c607949d0001
ALEX SLYE <slyealex@live.com>
2016-04-14 04:41:15 -0400

    add secure password to user model, started form for user sign up, created user controller methods for create and new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
   def new
-  end
-
-  def edit
+    @user = User.new
   end
 
   def index
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path notice: 'You have signed up!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :firstname, :lastname, :password, :password_confirmation)
+  end
+
 end
